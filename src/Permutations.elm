d9bba4142a9f309f567095e22bc7e2dba2f4acca
Ed Kelly <edwardjkelly@gmail.com>
2024-04-13 22:31:52 +0100

    Random-access permutations!

--- src/Permutations.elm
+++ src/Permutations.elm
@@ -1,13 +1,27 @@
 module Permutations exposing
-    ( bool
+    ( Definition
+    , Generator
+    , always
+    , append
+    , array
+    , bool
+    , char
     , customType
     , dict
+    , empty
     , field
     , float
+    , fromList
     , int
     , list
+    , map
+    , maybe
+    , new
+    , once
     , record
+    , result
     , string
+    , triple
     , tuple
     , unit
     , variant0
@@ -18,168 +32,402 @@ module Permutations exposing
     , variant5
     )
 
+import Array
 import Dict
-import Gen
 
 
-bool : Gen.GenTools (List Bool) Bool
+bool : Generator Bool
 bool =
-    Gen.fromList [ False, True ]
+    fromList [ False, True ]
 
 
-unit : Gen.GenTools (List ()) ()
+maybe : Generator a -> Generator (Maybe a)
+maybe a =
+    customType
+        |> variant0 Nothing
+        |> variant1 Just a
+
+
+result : Generator error -> Generator value -> Generator (Result error value)
+result x a =
+    customType
+        |> variant1 Err x
+        |> variant1 Ok a
+
+
+list : Int -> Generator a -> Generator (List a)
+list maxLength item =
+    let
+        countHelper acc n =
+            if n < 0 then
+                acc
+
+            else
+                countHelper ((item.count ^ n) + acc) (n - 1)
+
+        count =
+            countHelper 0 maxLength
+    in
+    new
+        { count = count
+        , nth =
+            \m ->
+                let
+                    n =
+                        m + 1
+                in
+                if n > count then
+                    Nothing
+
+                else
+                    let
+                        rotationsHelp periods length =
+                            if n <= List.sum ((item.count ^ length) :: periods) then
+                                List.foldl
+                                    (\period ( n_, output ) ->
+                                        ( modBy period n_
+                                        , n_ // period :: output
+                                        )
+                                    )
+                                    ( (n - 1) - List.sum periods, [] )
+                                    periods
+                                    |> Tuple.second
+
+                            else
+                                rotationsHelp (item.count ^ length :: periods) (length + 1)
+
+                        rotations =
+                            rotationsHelp [] 0
+                    in
+                    List.foldl
+                        (\rs mList ->
+                            case mList of
+                                Nothing ->
+                                    Nothing
+
+                                Just list_ ->
+                                    case item.nth rs of
+                                        Just v1 ->
+                                            Just (v1 :: list_)
+
+                                        Nothing ->
+                                            Nothing
+                        )
+                        (Just [])
+                        rotations
+        }
+
+
+unit : Generator ()
 unit =
-    Gen.once ()
+    once ()
 
 
-int : Gen.GenTools (List Int) Int
+int : Generator Int
 int =
-    Gen.fromList [ 0, -1, 1, 2, 3, -10, 10, -100, 100 ]
+    fromList [ 0, -1, 1, 2, 3, -10, 10, -100, 100 ]
 
 
-float : Gen.GenTools (List Float) Float
+float : Generator Float
 float =
-    Gen.fromList [ 0.0, -1.0, 1.0, 0.2, 0.3 ]
+    fromList [ 0.0, -1.0, 1.0, 0.2, 0.3 ]
 
 
-string : Gen.GenTools (List String) String
+string : Generator String
 string =
-    Gen.fromList [ "", " ", "\n", "\u{000D}", "\t", "\"", "a", "ab", "abc" ]
+    fromList [ "", " ", "\n", "\u{000D}", "\t", "\"", "a", "ab", "abc" ]
 
 
-list :
-    { minLength : Int, maxLength : Int }
-    -> Gen.GenTools state value
-    -> Gen.GenTools (Maybe (List state)) (List value)
-list { minLength, maxLength } =
-    Gen.list minLength maxLength
+char : Generator Char
+char =
+    fromList [ ' ', 'a', 'A', '0', '\n', '\t', '\u{000D}' ]
 
 
 tuple :
-    Gen.GenTools state1 value1
-    -> Gen.GenTools state2 value2
-    -> Gen.GenTools ( ( List (value1 -> value2 -> ( value1, value2 )), state1 ), state2 ) ( value1, value2 )
+    Generator value1
+    -> Generator value2
+    -> Generator ( value1, value2 )
 tuple fst snd =
     record Tuple.pair
         |> field fst
         |> field snd
 
 
+triple :
+    Generator value1
+    -> Generator value2
+    -> Generator value3
+    -> Generator ( value1, value2, value3 )
+triple fst snd thd =
+    record (\a b c -> ( a, b, c ))
+        |> field fst
+        |> field snd
+        |> field thd
+
+
+array : Int -> Generator a -> Generator (Array.Array a)
+array maxLength a =
+    list maxLength a
+        |> map Array.fromList
+
+
 dict :
-    { minLength : Int, maxLength : Int }
-    -> Gen.GenTools state1 comparable
-    -> Gen.GenTools state2 value
-    -> Gen.GenTools (Maybe (List ( ( List (comparable -> value -> ( comparable, value )), state1 ), state2 ))) (Dict.Dict comparable value)
-dict size k v =
-    list size (tuple k v)
-        |> Gen.map Dict.fromList
+    Int
+    -> Generator comparable
+    -> Generator value
+    -> Generator (Dict.Dict comparable value)
+dict maxLength k v =
+    list maxLength (tuple k v)
+        |> map Dict.fromList
 
 
-record : a -> Gen.GenTools (List a) a
+record : constructor -> Generator constructor
 record constructor =
-    Gen.once constructor
+    new
+        { count = 1
+        , nth = \n -> Just constructor
+        }
 
 
-field : Gen.GenTools state2 value1 -> Gen.GenTools state1 (value1 -> value2) -> Gen.GenTools ( state1, state2 ) value2
-field fieldType builder =
-    Gen.andMap fieldType builder
+field :
+    Generator value1
+    -> Generator (value1 -> value2)
+    -> Generator value2
+field gen builder =
+    new
+        { count = gen.count * builder.count
+        , nth =
+            \n ->
+                let
+                    rotations =
+                        n // builder.count
+
+                    remainder =
+                        modBy builder.count n
+                in
+                case ( builder.nth remainder, gen.nth rotations ) of
+                    ( Just left, Just right ) ->
+                        Just (left right)
+
+                    _ ->
+                        Nothing
+        }
 
 
-customType : Gen.GenTools () a
+customType : Generator a
 customType =
-    Gen.empty
+    empty
 
 
 variant0 :
     variant
-    -> Gen.GenTools builderState variant
-    -> Gen.GenTools ( builderState, List variant ) variant
+    -> Generator variant
+    -> Generator variant
 variant0 variant builder =
-    Gen.append builder (Gen.once variant)
+    append builder (once variant)
 
 
 variant1 :
     (arg -> variant)
-    -> Gen.GenTools argState arg
-    -> Gen.GenTools builderState variant
-    -> Gen.GenTools ( builderState, argState ) variant
+    -> Generator arg
+    -> Generator variant
+    -> Generator variant
 variant1 tag arg builder =
     arg
-        |> Gen.map tag
-        |> Gen.append builder
+        |> map tag
+        |> append builder
 
 
 variant2 :
     (arg1 -> arg2 -> variant)
-    -> Gen.GenTools arg1State arg1
-    -> Gen.GenTools arg2State arg2
-    -> Gen.GenTools builderState variant
-    -> Gen.GenTools ( builderState, ( ( List (arg1 -> arg2 -> variant), arg1State ), arg2State ) ) variant
+    -> Generator arg1
+    -> Generator arg2
+    -> Generator variant
+    -> Generator variant
 variant2 tag arg1 arg2 builder =
     let
         variantGen =
-            Gen.once tag
-                |> Gen.andMap arg1
-                |> Gen.andMap arg2
+            record tag
+                |> field arg1
+                |> field arg2
     in
-    Gen.append builder variantGen
+    append builder variantGen
 
 
 variant3 :
     (arg1 -> arg2 -> arg3 -> variant)
-    -> Gen.GenTools arg1State arg1
-    -> Gen.GenTools arg2State arg2
-    -> Gen.GenTools arg3State arg3
-    -> Gen.GenTools builderState variant
-    -> Gen.GenTools ( builderState, ( ( ( List (arg1 -> arg2 -> arg3 -> variant), arg1State ), arg2State ), arg3State ) ) variant
+    -> Generator arg1
+    -> Generator arg2
+    -> Generator arg3
+    -> Generator variant
+    -> Generator variant
 variant3 tag arg1 arg2 arg3 builder =
     let
         variantGen =
-            Gen.once tag
-                |> Gen.andMap arg1
-                |> Gen.andMap arg2
-                |> Gen.andMap arg3
+            record tag
+                |> field arg1
+                |> field arg2
+                |> field arg3
     in
-    Gen.append builder variantGen
+    append builder variantGen
 
 
 variant4 :
     (arg1 -> arg2 -> arg3 -> arg4 -> variant)
-    -> Gen.GenTools arg1State arg1
-    -> Gen.GenTools arg2State arg2
-    -> Gen.GenTools arg3State arg3
-    -> Gen.GenTools arg4State arg4
-    -> Gen.GenTools builderState variant
-    -> Gen.GenTools ( builderState, ( ( ( ( List (arg1 -> arg2 -> arg3 -> arg4 -> variant), arg1State ), arg2State ), arg3State ), arg4State ) ) variant
+    -> Generator arg1
+    -> Generator arg2
+    -> Generator arg3
+    -> Generator arg4
+    -> Generator variant
+    -> Generator variant
 variant4 tag arg1 arg2 arg3 arg4 builder =
     let
         variantGen =
-            Gen.once tag
-                |> Gen.andMap arg1
-                |> Gen.andMap arg2
-                |> Gen.andMap arg3
-                |> Gen.andMap arg4
+            record tag
+                |> field arg1
+                |> field arg2
+                |> field arg3
+                |> field arg4
     in
-    Gen.append builder variantGen
+    append builder variantGen
 
 
 variant5 :
     (arg1 -> arg2 -> arg3 -> arg4 -> arg5 -> variant)
-    -> Gen.GenTools arg1State arg1
-    -> Gen.GenTools arg2State arg2
-    -> Gen.GenTools arg3State arg3
-    -> Gen.GenTools arg4State arg4
-    -> Gen.GenTools arg5State arg5
-    -> Gen.GenTools builderState variant
-    -> Gen.GenTools ( builderState, ( ( ( ( ( List (arg1 -> arg2 -> arg3 -> arg4 -> arg5 -> variant), arg1State ), arg2State ), arg3State ), arg4State ), arg5State ) ) variant
+    -> Generator arg1
+    -> Generator arg2
+    -> Generator arg3
+    -> Generator arg4
+    -> Generator arg5
+    -> Generator variant
+    -> Generator variant
 variant5 tag arg1 arg2 arg3 arg4 arg5 builder =
     let
         variantGen =
-            Gen.once tag
-                |> Gen.andMap arg1
-                |> Gen.andMap arg2
-                |> Gen.andMap arg3
-                |> Gen.andMap arg4
-                |> Gen.andMap arg5
+            record tag
+                |> field arg1
+                |> field arg2
+                |> field arg3
+                |> field arg4
+                |> field arg5
     in
-    Gen.append builder variantGen
+    append builder variantGen
+
+
+type alias Generator value =
+    { count : Int
+    , nth : Int -> Maybe value
+    , all : () -> List value
+    , every : Int -> List value
+    , sample : Float -> List value
+    }
+
+
+type alias Definition value =
+    { count : Int
+    , nth : Int -> Maybe value
+    }
+
+
+new : Definition value -> Generator value
+new definition =
+    let
+        all () =
+            List.range 0 definition.count
+                |> List.filterMap definition.nth
+
+        every i =
+            everyHelp [] 0 i
+                |> List.reverse
+
+        everyHelp output n i =
+            if n > toFloat definition.count then
+                output
+
+            else
+                case definition.nth (round n) of
+                    Nothing ->
+                        everyHelp output (n + i) (max 1 i)
+
+                    Just nth ->
+                        everyHelp (nth :: output) (n + i) (max 1 i)
+
+        sample pct =
+            let
+                clampedPct =
+                    clamp 0.0 1.0 pct
+            in
+            if clampedPct == 0 then
+                []
+
+            else
+                (toFloat definition.count / (toFloat definition.count * clampedPct))
+                    |> every
+    in
+    { count = definition.count
+    , nth = definition.nth
+    , all = all
+    , every = toFloat >> every
+    , sample = sample
+    }
+
+
+empty : Generator a
+empty =
+    new
+        { count = 0
+        , nth = \_ -> Nothing
+        }
+
+
+always : a -> Generator a
+always value =
+    new
+        { count = 1
+        , nth = \_ -> Just value
+        }
+
+
+once : a -> Generator a
+once value =
+    fromList [ value ]
+
+
+fromList : List a -> Generator a
+fromList list_ =
+    let
+        array_ =
+            Array.fromList list_
+    in
+    new
+        { count = List.length list_
+        , nth = \n -> Array.get n array_
+        }
+
+
+map :
+    (value1 -> value2)
+    -> Generator value1
+    -> Generator value2
+map f gen =
+    new
+        { count = gen.count
+        , nth = \n -> Maybe.map f (gen.nth n)
+        }
+
+
+append :
+    Generator value
+    -> Generator value
+    -> Generator value
+append left right =
+    new
+        { count = left.count + right.count
+        , nth =
+            \n ->
+                if n < left.count then
+                    left.nth n
+
+                else
+                    right.nth (n - left.count)
+        }
