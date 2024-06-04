eb2a8515a4cef52fc0145fd146db230e46af9b42
edkelly303 <edwardjkelly@gmail.com>
2024-06-04 13:27:07 +0100

    Update imports in tests

--- tests/Example.elm
+++ tests/Example.elm
@@ -1,14 +1,14 @@
 module Example exposing (..)
 
 import Expect
-import Permutations as P
+import Exhaustive as E
 import Test exposing (..)
 
 
 list =
-    P.list 10 (P.oneOf [ 1, 2, 3, 4, 5, 6, 7, 8, 9 ])
+    E.list 10 (E.oneOf [ 1, 2, 3, 4, 5, 6, 7, 8, 9 ])
 
 
 suite : Test
 suite =
-    P.test list "List isn't [ 1, 2, 3, 4, 5 ]" <| \l -> l |> Expect.notEqual [ 1, 2, 3, 4, 5 ]
+    E.test list "List isn't [ 1, 2, 3, 4, 5 ]" <| \l -> l |> Expect.notEqual [ 1, 2, 3, 4, 5 ]
