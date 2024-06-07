5085b451ba293afa675f82259fac9d1e061e1455
Ed <edwardjkelly@gmail.com>
2024-06-07 18:21:24 +0100

    Work on docs

--- src/Exhaustive.elm
+++ src/Exhaustive.elm
@@ -84,51 +84,148 @@ test gen description toExpectation =
         (\() -> helper 0)
 
 
-{-| -}
+{-| Generate all values of the unit type, `()`. There's only one!
+-}
 unit : Generator ()
 unit =
     constant ()
 
 
+{-| Generate all boolean values. The 0th term is `False`, the 1st term is `True`.
+-}
 bool : Generator Bool
 bool =
     values [ False, True ]
 
+{-| Generate integer values. 
 
+Since the set of all integers is infinite, we don't 
+attempt to generate all of them. Instead, we try a small set of potentially 
+interesting integers, which may be useful in tests: 
+
+    import Exhaustive exposing (int)
+
+    int.all ()
+
+    --> [ 0, -1, 1, 2, 3, -10, 10, -100, 100 ]
+
+(I am open to suggestions for more interesting integers than these!)
+
+If you want to generate a specific list of integers, see the docs for `values` 
+instead.
+-}
 int : Generator Int
 int =
     values [ 0, -1, 1, 2, 3, -10, 10, -100, 100 ]
 
+{-| Generate `Float` values.
 
+Since the set of all floats is infinite, we don't 
+attempt to generate all of them. Instead, we try a small set of potentially 
+interesting floats, which may be useful in tests: 
+
+    import Exhaustive exposing (float)
+
+    float.all ()
+
+    --> [ 0.0, -1.0, 1.0, 0.2, 0.3, 0/0, 1/0 ]
+
+(I am open to suggestions for more interesting floats than these!)
+
+If you want to generate a specific list of floats, see the docs for `values` 
+instead.
+-}
 float : Generator Float
 float =
-    values [ 0.0, -1.0, 1.0, 0.2, 0.3 ]
+    values [ 0.0, -1.0, 1.0, 0.2, 0.3, 0/0, 1/0 ]
 
+{-| Generate `String` values.
 
+Since the set of all `String`s is infinite, we don't 
+attempt to generate all of them. Instead, we try a small set of potentially 
+interesting `String`s, which may be useful in tests: 
+
+    import Exhaustive exposing (string)
+
+    string.all ()
+
+    --> [ "", " ", "\n", "\u{000D}", "\t", "\"", "a", "ab", "abc" ]
+
+(I am open to suggestions for more interesting `String`s than these!)
+
+If you want to generate a specific list of `String`s, see the docs for `values` 
+instead.
+-}
 string : Generator String
 string =
     values [ "", " ", "\n", "\u{000D}", "\t", "\"", "a", "ab", "abc" ]
 
+{-| Generate `Char` values.
 
+Since the set of all `Char`s is infinite, we don't 
+attempt to generate all of them. Instead, we try a small set of potentially 
+interesting `Char`s, which may be useful in tests: 
+
+    import Exhaustive exposing (char)
+
+    char.all ()
+
+    --> [ ' ', 'a', 'A', '0', '\n', '\t', '\u{000D}' ]
+
+(I am open to suggestions for more interesting `Char`s than these!)
+
+If you want to generate a specific list of `Char`s, see the docs for `values` 
+instead.
+-}
 char : Generator Char
 char =
     values [ ' ', 'a', 'A', '0', '\n', '\t', '\u{000D}' ]
 
+{-| Generate all `Maybe` values of a given type.
 
+    import Exhaustive exposing (maybe, bool)
+
+    maybeBool = 
+        maybe bool
+
+    maybeBool.all ()
+
+    --> [ Nothing, Just False, Just True ]
+-}
 maybe : Generator a -> Generator (Maybe a)
 maybe a =
     customType
         |> variant0 Nothing
         |> variant1 Just a
 
+{-| Generate all `Result` values of given error and ok types.
 
+    import Exhaustive exposing (result, unit, bool)
+
+    resultUnitBool = 
+        result unit bool
+
+    resultUnitBool.all ()
+
+    --> [ Err (), Ok False, Ok True ]
+-}
 result : Generator error -> Generator value -> Generator (Result error value)
 result x a =
     customType
         |> variant1 Err x
         |> variant1 Ok a
 
+{-| Generate all pairs of values of given types.
 
+    import Exhaustive exposing (pair, bool)
+
+    pairBool = 
+        pair bool bool
+
+    pairBool.all ()
+
+    --> [ ( False, False ), ( True, False ), ( False, True ), ( True, True ) ]
+-}
 pair :
     Generator value1
     -> Generator value2
@@ -138,7 +235,17 @@ pair fst snd =
         |> field fst
         |> field snd
 
+{-| Generate all triples of values of given types.
 
+    import Exhaustive exposing (triple, bool, unit)
+
+    tripleBoolUnitUnit = 
+        triple bool unit unit
+
+    tripleBoolUnitUnit.all ()
+
+    --> [ ( False, (), () ), ( True, (), () ) ]
+-}
 triple :
     Generator value1
     -> Generator value2
@@ -150,7 +257,18 @@ triple fst snd thd =
         |> field snd
         |> field thd
 
+{-| Generate all lists of values of a given type, starting with the empty list 
+`[]`, and continuing up to lists of a specified length.
 
+    import Exhaustive exposing (list, bool)
+
+    listBool = 
+        list 1 bool
+
+    listBool.all ()
+
+    --> [ [], [ False ], [ True ] ]
+-}
 list : Int -> Generator a -> Generator (List a)
 list maxLength item =
     let
@@ -206,13 +324,40 @@ list maxLength item =
                         rotations
         }
 
+{-| Generate all `Array`s of a given type, starting with an empty `Array` and 
+continuing up to `Array`s of a specified length.
 
+    import Exhaustive exposing (array, bool)
+    import Array
+
+    arrayBool = 
+        array 1 bool
+
+    arrayBool.all ()
+
+    --> [ Array.fromList [], Array.fromList [ False ], Array.fromList [ True ] ]
+
+-}
 array : Int -> Generator a -> Generator (Array.Array a)
 array maxLength a =
     list maxLength a
         |> map Array.fromList
 
+{-| Generate all `Dict`s of a given key and value type, starting with an empty 
+`Dict` and continuing up to `Dict`s of a specified length.
 
+    import Exhaustive exposing (dict, values, unit)
+    import Dict
+
+    myDict = 
+        dict 1 
+            (values [ "a", "b" ])
+            unit
+
+    myDict.all ()
+
+    --> [ Dict.fromList [], Dict.fromList [ ( "a", () ) ], Dict.fromList [ ( "b", () ) ] ]
+-}
 dict :
     Int
     -> Generator comparable
