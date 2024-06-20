d5a6fa1a7f0e572a2224ec3150018de34ad75e34
Ed <edwardjkelly@gmail.com>
2024-06-20 12:05:27 +0100

    Delete stupid tests

--- tests/Example.elm
+++ tests/Example.elm
@@ -1,32 +1,9 @@
 module Example exposing (..)
 
-import Exhaustive as E
-import Expect
 import Test exposing (..)
-
-
-list =
-    E.list 10 (E.values [ 1, 2, 3, 4, 5, 6, 7, 8, 9 ])
-
-
-monad =
-    E.values [ "a", "b", "c" ]
-        |> E.andThen
-            (\str ->
-                if str == "a" then
-                    E.values [ 1, 2, 3, 4 ]
-
-                else if str == "b" then
-                    E.values [ 5, 6, 7, 8, 9 ]
-
-                else
-                    E.values [ 10, 11 ]
-            )
 
 
 suite : Test
 suite =
     describe "Tests!"
-        [ E.test list "List isn't [ 1, 2, 3, 4, 5 ]" <| \l -> l |> Expect.notEqual [ 1, 2, 3, 4, 5 ]
-        , E.test monad "Monad can't be 11" <| \m -> m |> Expect.notEqual 11
-        ]
+        []
